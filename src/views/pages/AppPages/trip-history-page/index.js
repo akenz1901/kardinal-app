150d6d85b6b0f09fb7d61c5f6deeff0c1598b7e1
akenz1901 <michael_aka1@icloud.com>
2022-03-17 15:50:11 +0100

    Modified template

--- src/views/pages/AppPages/trip-history-page/index.js
+++ src/views/pages/AppPages/trip-history-page/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import NavBar from "../../../components/user/NavBar";
 import TripHistoryComponent from "../../../components/user/trip-history";
 
 function TripHistoryPage(){
     return(
+        <>
+        <NavBar/>
         <TripHistoryComponent />
+        </>
     )
 }
 
